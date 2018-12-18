db78dd67e7c97773740aaa3ce8e267911757bb86
Cian Durrigan <cian@adaptis.io>
2018-12-18 11:50:47 +0700

    lifecycle methods - add componnentDidMount with getPosition code & refactor constructor function to one line

--- src/index.js
+++ src/index.js
@@ -3,31 +3,29 @@ import ReactDOM from 'react-dom';
 
 // App subclasses React.Component, which gives App access to all of React.Component methods
 class App extends React.Component {
+
   // constructor fucntion specific to JS, not specific to React
   // constructor function is automatically and instantly called before anything else when App is called
-  constructor(props) {
-    // super is a reference to the parents(React.Component) constructor function
-    // have to do it every time we define the constructor function inside the component
-    super(props);
+  // good convention - do not do data loading inside the constructor function
+  // OPTIONAL
+  // because react runs code through Babel, this line compiles to create constructor function
+  // shorthand for the costructor function
+  state = { lat: null, errorMessage: '' };
 
-    // THIS IS THE ONLY TIME we do direct assigment to this.state
-    this.state = { lat: null, errorMessage: '' };
-
+  // run one time when the app is launched(initially called from ReactDOM.render)
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      position => {
-        // to update our state object we called setState!!!
-        this.setState({ lat: position.coords.latitude });
-      },
-      err => {
-        // setState is an additive process, so we only need to update what we need
-        // it wont overwrite previously set values
-        // it will leave lat: untouched
-        this.setState({ errorMessage: err.message});
-      }
+      // to update our state object we called setState!!!
+      position => this.setState({ lat: position.coords.latitude }),
+      // setState is an additive process, so we only need to update what we need
+      // it wont overwrite previously set values
+      // it will leave lat: untouched
+      err => this.setState({ errorMessage: err.message})
     );
   }
 
   // React says we have to define render() {}
+  // this just returns JSX
   render() {
     // conditional rendering
     if(this.state.errorMessage && !this.state.lat) {
